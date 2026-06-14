795faedf1ddc8b7a5e7391a226613dd630674621
Ahmed Sallam <87598902+ahmedsallam97@users.noreply.github.com>
2026-06-14 22:49:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,50 @@
 async function login(){
 
-document.getElementById("message").innerText =
-"سيتم تفعيل تسجيل الدخول في الخطوة القادمة";
+  const username =
+    document.getElementById("username").value;
+
+  const password =
+    document.getElementById("password").value;
+
+  const message =
+    document.getElementById("message");
+
+  message.innerText = "جاري تسجيل الدخول...";
+
+  try{
+
+    const response = await fetch(
+      `${API_URL}?action=login&username=${username}&password=${password}`
+    );
+
+    const data = await response.json();
+
+    if(data.success){
+
+      localStorage.setItem(
+        "currentUser",
+        JSON.stringify(data.user)
+      );
+
+      message.innerText =
+        "تم تسجيل الدخول";
+
+      setTimeout(()=>{
+        window.location.href="dashboard.html";
+      },1000);
+
+    }else{
+
+      message.innerText =
+        "اسم المستخدم أو كلمة المرور غير صحيحة";
+
+    }
+
+  }catch(err){
+
+    message.innerText =
+      "خطأ في الاتصال بالخادم";
+
+  }
 
 }
